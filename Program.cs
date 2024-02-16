64a0255a1ba914bf6aa748600455beceb7c50c95
TatikondaAnvitha123 <tatikondaanvitha05@gmail.com>
2024-02-16 09:41:47 +0530

    All Files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Welcome to number system");
+Console.WriteLine("*** Even Numbers***");
+NumberSystem.EvenNum();
+Console.WriteLine("*** Odd Numbers ***");
+NumberSystem.OddNum();
